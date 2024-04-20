083e0fe90ac2e709f117e0a967bfe7d8afa8c3f2
mojtaba_ni <mojtabanik@yahoo.com>
2024-04-20 22:59:19 +0330

    add language

--- src/shared/language/index.js
+++ src/shared/language/index.js
@@ -1,4 +1,93 @@
 export const strings = {
-    home : "خانه",
-    lenz : "لنز طبی",
-}
+  successAlert:"عملیات با موفقیت انجام شد",
+  add:"افزودن",
+  submit:"ثبت",
+  navbar: {
+    home: "خانه",
+    medicalLens: "لنز طبی",
+    ColoredLens: "لنز رنگی",
+    accessories: "محصولات جانبی",
+    cosmetic: "آرایشی",
+    blog: "بلاگ",
+    blogVideo: "ویدیو بلاگ",
+    lenzType: "نوع لنز",
+    medical: "طبی",
+    medicalColor: "طبی رنگ",
+    astigmat: "آستیگمات",
+    hard: "هارد",
+    brand : "برند",
+    Period: "دوره مصرف",
+    arian: "آرین",
+    elamor: "الامور",
+    daily:"روزانه",
+    monthly:"ماهانه",
+    yearly:"سالانه",
+    seasonal:"فصلی"
+  },
+  landing: {
+    bestSellers: "پر فروش ترین ها",
+    brands: "برند ها",
+    questions: "سوالات متداول",
+    educationCv: "مقالات آموزشی",
+  },
+  educationCard: {
+    continue: "ادامه مطلب",
+  },
+  register: {
+    title: "ثبت نام",
+    submit: "ثبت",
+  },
+  panel :{
+    allUserPage :{
+      username:"نام کاربری",
+      phoneNumber:"شماره تلفن",
+      createdAt:"تاریخ ثبت نام",
+    },
+    blog:{
+      blogTitle:"عنوان بلاگ",
+      introduction:"مقدمه",
+      createdAt:"تاریخ ایجاد",
+    },
+    category:{
+      title:"نام دسته",
+      introduction:"مقدمه",
+      createdAt:"تاریخ ایجاد",
+    },
+    brand:{
+      brandTitle:"نام برند",
+      introduction:"مقدمه",
+      createdAt:"تاریخ ایجاد",
+    },
+    product:{
+      name:"نام محصول",
+      description:"توضیحات",
+      Specifications:"مشخصات فنی",
+      price:"قیمت",
+    },
+  },
+  profile:{
+    addressTitle: "آدرس های فعلی شما",
+    orders:"سفارشات",
+    address:"آدرس",
+    favorite:"علاقه مندی ها",
+    logout:"خروج از حساب کاربری",
+    account:"اطلاعات حساب",
+    info:"پروفایل کاربری",
+    addNewAddress:"افزودن آدرس جدید",
+    addAddress:"افزودن آدرس ",
+    zipCode:"کد پستی",
+    errorMessage:{
+      zipCodeError:"کد پستی را وارد کنید",
+      numberError:"پلاک را وارد کنید",
+      productNameError:"اسم محصول را وارد کنید",
+      descriptionError:"توضیحات را وارد کنید",
+      uploadError:"عکس محصول را وارد کنید",
+      SpecificationsError:"مشخصات فنی را وارد کنید",
+      categoryError:"اسم دسته را وارد کنید",
+      stepError:"اسم زیر دسته را وارد کنید",
+    }
+  },
+  brand:{
+    addTitle:"افزودن برند"
+  }
+};
