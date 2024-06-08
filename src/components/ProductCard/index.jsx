de9a4de2228ceb262935ba7576f10fd38a222487
mojtaba_ni <mojtabanik@yahoo.com>
2024-06-08 21:05:58 +0330

    feat: add Link to productCard

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -1,19 +1,19 @@
 /* eslint-disable react/prop-types */
-import React from 'react'
 import boxImg from "../../assets/images/jabe.png"
 import { Card } from "antd";
 import styles from "./productCard.module.css"
 import {
   StarFilled
 } from "@ant-design/icons";
+import { Link } from 'react-router-dom';
 
-
-const ProductCard = ({data}) => {
+const ProductCard = ({productInfo}) => {
   return (
-    <Card className={styles.ProductContainer} hoverable>
-        <img src={data ? data?.image :  boxImg} alt="jabe" className={styles.ProductImg} />
+    <Link to={`/product/${productInfo?._id}`}>
+     <Card className={styles.ProductContainer} hoverable>
+        <img src={productInfo ? productInfo?.image :  boxImg} alt="jabe" className={styles.ProductImg} />
         <div className={styles.ProductCardDesc}>
-        <h4>{data ? data?.name : "لنز طبی جانسون اکیو ویو اویسس"}</h4>
+        <h4>{productInfo?.name}</h4>
         <div className={styles.rateBox}>
         <StarFilled style={{color:"#F0DB23" , fontSize:"1.2rem"}} />
         <StarFilled style={{color:"#F0DB23" , fontSize:"1.2rem"}} />
@@ -22,10 +22,12 @@ const ProductCard = ({data}) => {
         <StarFilled style={{color:"#e3e3e3" , fontSize:"1.2rem"}} />
         </div>
        
-        <strong>{data ? `${data?.price} تومان` : "485000 تومان"}</strong>
+        <strong>{productInfo ? `${productInfo?.price} تومان` : "485000 تومان"}</strong>
         </div>
        
     </Card>
+    </Link>
+   
   )
 }
 
