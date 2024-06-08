28e002d803640e60e858da56bd15889714f989d4
mojtaba_ni <mojtabanik@yahoo.com>
2024-06-08 21:02:36 +0330

    feat: fetchData in bestPart component

--- src/components/Bestpart/index.jsx
+++ src/components/Bestpart/index.jsx
@@ -1,17 +1,19 @@
 /* eslint-disable react/prop-types */
-import React from "react";
+
 import styles from "./bestPart.module.css";
-import { strings } from "../../shared/language";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { FreeMode, Pagination } from "swiper/modules";
+import { FreeMode } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/free-mode";
 import "swiper/css/pagination";
-import 'swiper/css/navigation';
+import "swiper/css/navigation";
 import ProductCard from "../ProductCard";
-import { Navigation } from 'swiper/modules';
+import { Navigation } from "swiper/modules";
+import { Col, Row, Skeleton } from "antd";
 
-const Bestpart = ({ title }) => {
+const Bestpart = ({ title, data }) => {
+  const arrSkeleton = [0, 1, 2, 4];
+  console.log({data});
   return (
     <div className={styles.bestSellerBox}>
       <div>
@@ -23,34 +25,51 @@ const Bestpart = ({ title }) => {
           slidesPerView={4}
           spaceBetween={30}
           freeMode={true}
-          navigation={true} 
+          navigation={true}
           modules={[FreeMode, Navigation]}
           className="mySwiper"
-          
           style={{
-            minHeight:"240px",
+            minHeight: "200px",
             "--swiper-button-next": { left: "-5px" },
             "--swiper-pagination-bullet-inactive-color": "#999999",
             "--swiper-pagination-bullet-inactive-opacity": "1",
             "--swiper-pagination-bullet-size": "16px",
-            "--swiper-pagination-bullet-horizontal-gap": "10px"
-        }}
+            "--swiper-pagination-bullet-horizontal-gap": "10px",
+          }}
         >
-          <SwiperSlide style={{display:"flex" , justifyContent:"center" , margin:" 0px !important"}}>
-            <ProductCard />
-          </SwiperSlide>
-          <SwiperSlide style={{display:"flex" , justifyContent:"center" , margin:" 0px !important"}}>
-            <ProductCard />
-          </SwiperSlide>
-          <SwiperSlide style={{display:"flex" , justifyContent:"center" , margin:" 0px !important"}}>
-            <ProductCard />
-          </SwiperSlide>
-          <SwiperSlide style={{display:"flex" , justifyContent:"center" ,margin:" 0px !important"}}>
-            <ProductCard />
-          </SwiperSlide>
-          <SwiperSlide style={{display:"flex" , justifyContent:"center" , margin:" 0px !important"}}>
-            <ProductCard />
-          </SwiperSlide>
+          {data?.length === 0 ? (
+            <Row justify="space-evenly">
+              {arrSkeleton.map((item, index) => (
+                <Col
+                style={{display:"flex" , justifyContent:"center" , alignItems:"center"}}
+                  key={index}
+                  className={`gutter-row ${styles.rowCol} `}
+                  md={6}
+                  >
+                  <Skeleton.Input
+                    style={{
+                      marginBottom: ".5rem",
+                      minHeight: "230px",
+                      minWidth: "270px",
+                    }}
+                  />
+                </Col>
+              ))}
+            </Row>
+          ) : (
+            data?.map((item, index) => (
+              <SwiperSlide
+                key={index}
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  margin: " 0px !important",
+                }}
+              >
+                <ProductCard productInfo={item} />
+              </SwiperSlide>
+            ))
+          )}
         </Swiper>
       </div>
     </div>
