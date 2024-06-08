6ad3343e36c7dbc44ceabfb0a71ba2189b8ea129
mojtaba_ni <mojtabanik@yahoo.com>
2024-06-08 21:06:29 +0330

    feat: create themeContext

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,27 @@ import ProductList from "./page/product/productList";
 import BrandList from "./page/product/brandList";
 import PeriodList from "./page/product/periodList";
 import Search from "./page/search";
+import useLocalStorage from 'use-local-storage'
+import "./index.css"
+import Map from "./page/map";
+import MapPage from "./page/map";
+import VideoBlogList from "./page/blog/video";
+import VBlogSingle from "./page/blog/video/vBlogSingle";
+import Order from "./page/order";
+import LenzTest from "./page/lenzTest";
+
 
 export default function App() {
+  const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const [theme, setTheme] = useLocalStorage('theme', defaultDark ? 'dark' : 'light');
+
+const switchTheme = () => {
+  const newTheme = theme === 'light' ? 'dark' : 'light'
+  setTheme(newTheme )
+}
+
   return (
-    <>
+    <div data-theme={theme} className="app"> 
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
@@ -28,9 +45,14 @@ export default function App() {
           <Route path="/brands/:brandId" element={<BrandList />} />
           <Route path="/periods/:periodId" element={<PeriodList />} />
           <Route path="/search/:name" element={<Search />} />
+          <Route path="/vlog" element={<VideoBlogList />} />
+          <Route path="/vlog/:id" element={<VBlogSingle />} />
+          <Route path="/map" element={<MapPage />} />
+          <Route path="/order" element={<Order />} />
+          <Route path="/lenzTest" element={<LenzTest />} />
         </Routes>
         <ToastContainer />
       </Router>
-    </>
+    </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,9 @@ import "./index.css";
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from "react-toastify";
 
+
+
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ConfigProvider direction="rtl">
