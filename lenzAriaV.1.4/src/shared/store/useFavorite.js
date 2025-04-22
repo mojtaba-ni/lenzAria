18a48202185bc07a6219cab11221f623790d1c22
mojtaba_ni <mojtabanik@yahoo.com>
2025-04-22 19:31:33 +0330

    fix: check config2

--- lenzAriaV.1.4/src/shared/store/useFavorite.js
+++
@@ -1,6 +0,0 @@
-import { create } from 'zustand'
-
-export const useFavorite = create((set) => ({
-  favoriteList: [],
-  updateFavoriteList: (data) => set((state) => ({ favoriteList : [...state.favoriteList,data]})),
-}))
--- lenzAriaV.1.4/src/shared/store/useSearch.js
+++
@@ -1,6 +0,0 @@
-import { create } from 'zustand'
-
-export const useSearch = create((set) => ({
-  title: null,
-  updateTitle: () => set((state) => ({ title: state })),
-}))
