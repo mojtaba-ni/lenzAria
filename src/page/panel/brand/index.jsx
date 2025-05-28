1abe3ed58f31c96abccb234e4cc382054c7b460f
mojtaba_ni <mojtabanik@yahoo.com>
2025-05-28 12:54:33 +0330

    feat: implement edit functionality for brand management with modal support

--- src/page/panel/brand/index.jsx
+++ src/page/panel/brand/index.jsx
@@ -3,10 +3,11 @@ import { useEffect, useState } from "react";
 import { strings } from "../../../shared/language";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import { DeleteTwoTone } from "@ant-design/icons";
+import { DeleteTwoTone, EditTwoTone } from "@ant-design/icons";
 import CustomModal from "../../../components/Modal";
-import {  dateFullFilter } from "../../../shared/utils";
+import { dateFullFilter } from "../../../shared/utils";
 import { path } from "../../../shared/config";
+import { toast } from "react-toastify";
 
 const Brand = () => {
   const [brand, setBrand] = useState();
@@ -15,15 +16,19 @@ const Brand = () => {
   const [update, setUpdate] = useState(false);
   const [showRemoveModal, setShowRemoveModal] = useState(false);
   const [record, setRecord] = useState();
-  
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+  const [editRecord, setEditRecord] = useState();
+  const [editForm] = Form.useForm();
+
+  const hideEditModal = () => {
+    setIsEditModalOpen(false);
+  };
+
   const showModal = () => {
     setIsModalOpen(true);
   };
   const geyAllBrand = async () => {
-    const { data } = await axios.get(
-      "${path}/api/brand/getAllBrand"
-    );
-
+    const { data } = await axios.get(`${path}/api/brand/getAllBrand`);
     setBrand(data?.data);
   };
 
@@ -33,21 +38,44 @@ const Brand = () => {
         name: newBrand,
       });
     }
+    toast.success(res?.data?.message);
     hideModal();
     setUpdate(!update);
   };
+
   const handleDeleteModal = (record) => {
     setShowRemoveModal(true);
     setRecord(record);
   };
+  const handleEditModal = (record) => {
+    setIsEditModalOpen(true);
+    setEditRecord(record);
+  };
 
   const handleDeleteBrand = async (id) => {
     const res = await axios.delete(`${path}/api/brand/delete`, {
-      data: { brandId: id},
+      data: { brandId: id },
     });
-
+    toast.success(res?.data?.message);
     setUpdate(!update);
     setShowRemoveModal(false);
+  };
+
+  const handleEdit = async (record) => {
+    const values = await editForm.validateFields();
+    const res = await axios.put(`${path}/api/brand/update`, {
+      brandId: record,
+      name: values?.editBrand,
+    });
+    if (res?.data?.isSuccess) {
+      toast.success(res?.data?.message);
+      setUpdate(!update);
+      setIsEditModalOpen(false);
+      setEditRecord(null);
+      editForm.resetFields();
+    } else {
+      toast.error(res?.data?.message);
+    }
   };
 
   useEffect(() => {
@@ -72,10 +100,8 @@ const Brand = () => {
       title: strings.panel.blog.createdAt,
       dataIndex: "createdAt",
       render: (_, record) => (
-        <Space size="middle">
-            {dateFullFilter(record?.createdAt)}
-        </Space>
-        ),
+        <Space size="middle">{dateFullFilter(record?.createdAt)}</Space>
+      ),
     },
     {
       title: "عملیات",
@@ -87,6 +113,12 @@ const Brand = () => {
               twoToneColor="#eb2f96"
               style={{ fontSize: "1.2rem" }}
               onClick={() => handleDeleteModal(record)}
+            />
+          </Link>
+          <Link>
+            <EditTwoTone
+              style={{ fontSize: "1.2rem" }}
+              onClick={() => handleEditModal(record)}
             />
           </Link>
         </Space>
@@ -157,6 +189,65 @@ const Brand = () => {
           </Form.Item>
         </Form>
       </Modal>
+      {isEditModalOpen && (
+        <Modal
+          title={strings.brand.editTitle}
+          open={isEditModalOpen}
+          onOk={() => handleEdit(editRecord?._id)}
+          onCancel={hideEditModal}
+          footer={[
+            <div
+              key={1}
+              style={{
+                display: "flex",
+                alignItems: "center",
+                gap: "1rem",
+                justifyContent: "end",
+              }}
+            >
+              <Button
+                key="back"
+                danger
+                onClick={hideEditModal}
+                style={{ width: "75px" }}
+              >
+                لغو
+              </Button>
+              <Button
+                key="submit"
+                type="primary"
+                onClick={() => handleEdit(editRecord?._id)}
+                style={{ width: "75px" }}
+              >
+                ثبت
+              </Button>
+            </div>,
+          ]}
+        >
+          <Form form={editForm}>
+            <Typography.Title level={5} style={{ width: "100px" }}>
+              نام برند
+            </Typography.Title>
+            <Form.Item
+              name="editBrand"
+              rules={[
+                {
+                  required: true,
+                  message: strings.profile.errorMessage.brandNameError,
+                },
+              ]}
+              style={{ width: "100%" }}
+            >
+              <Input
+                value={editRecord?.name}
+                onChange={(e) =>
+                  setEditRecord({ ...editRecord, name: e.target.value })
+                }
+              />
+            </Form.Item>
+          </Form>
+        </Modal>
+      )}
       <div
         style={{
           display: "flex",
